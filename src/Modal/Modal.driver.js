3fe0b5775b2b921b661cda552fd44ddf107b82b9
Guy Sopher <guyso@wix.com>
2017-03-16 13:14:29 +0200

    Add modal tests + fix flacky tests

--- src/Modal/Modal.driver.js
+++ src/Modal/Modal.driver.js
@@ -6,12 +6,18 @@ const modalDriverFactory = ({element, wrapper, component}) => {
 
   const getPortal = () => document.body.querySelector('.portal');
   const getOverlay = () => document.body.querySelector('.ReactModal__Overlay');
+  console.log(getOverlay());
+
   return {
     exists: () => !!(getPortal()) && !!element,
     isThemeExist: theme => !!getPortal().querySelector(`.${theme}`),
     getTitle: () => getPortal().querySelector('[data-hook="header-layout-title"]').textContent,
     getChildBySelector: selector => getPortal().querySelector(selector),
-    clickOnOverlay: () => ReactTestUtils.Simulate.click(getOverlay()),
+    clickOnOverlay: () => {
+      const overlay = getOverlay();
+      ReactTestUtils.Simulate.mouseDown(overlay);
+      ReactTestUtils.Simulate.mouseUp(overlay);
+    },
     setProps: props => {
       const ClonedWithProps = React.cloneElement(component, Object.assign({}, component.props, props), ...(component.props.children || []));
       ReactDOM.render(<div ref={r => element = r}>{ClonedWithProps}</div>, wrapper);
--- src/Modal/Modal.spec.js
+++ src/Modal/Modal.spec.js
@@ -3,23 +3,21 @@ import Modal from './Modal';
 import ModalFactory from './Modal.driver';
 import {createDriverFactory} from '../test-common';
 import sinon from 'sinon';
-/*
 import {isTestkitExists, isEnzymeTestkitExists} from '../../testkit/test-common';
 import {modalTestkitFactory} from '../../testkit';
 import {modalTestkitFactory as enzymeMessageBoxTestkitFactory} from '../../testkit/enzyme';
-*/
 
 /**
  * Modal.propTypes = {
   - isOpen: React.PropTypes.bool.isRequired,
   - contentLabel: React.PropTypes.string.isRequired,
-  onCancel: React.PropTypes.func,
-  style: React.PropTypes.oneOf(Object.keys(colors)),
-  theme: React.PropTypes.oneOf(Object.keys(colors)),
+  X onCancel: React.PropTypes.func,
+  X style: React.PropTypes.oneOf(Object.keys(colors)),
+  - theme: React.PropTypes.oneOf(Object.keys(colors)),
   - children: React.PropTypes.any,
   zIndex: React.PropTypes.number,
   - shouldCloseOnOverlayClick: React.PropTypes.bool,
-  onRequestClose: React.PropTypes.func,
+  - onRequestClose: React.PropTypes.func,
   - onAfterOpen: React.PropTypes.func,
   horizontalPosition: React.PropTypes.oneOf(Object.keys(positions)),
   verticalPosition: React.PropTypes.oneOf(Object.keys(positions)),
@@ -29,9 +27,11 @@ import {modalTestkitFactory as enzymeMessageBoxTestkitFactory} from '../../testk
 describe('Modal', () => {
   const createDriver = createDriverFactory(ModalFactory);
 
-  const props = {};
+  let props = {};
 
   beforeEach(() => {
+    document.body.innerHTML = ''; //remove previous modals from body
+    props = {};
     props.isOpen = true;
     props.contentLabel = 'modal_' + Math.random();
   });
@@ -41,6 +41,7 @@ describe('Modal', () => {
     it(`should not render the modal content if not open by default`, () => {
       props.isOpen = false;
 
+      console.log(props);
       const driver = createDriver(<Modal {...props}>
         <div data-hook="inner-div"/>
       </Modal>);
@@ -48,44 +49,85 @@ describe('Modal', () => {
     });
 
     it(`should render the passed children in the markup`, () => {
-      // props.isOpen = true;
-      // const driver = createDriver(<Modal {...props}>
-      //   <div className="inner-div" data-hook="inner-div"/>
-      // </Modal>);
-      // expect(driver.getChildBySelector('[data-hook="inner-div"]')).not.toBeNull();
+      props.isOpen = true;
+      console.log(props);
+      const driver = createDriver(<Modal {...props}>
+        <div data-hook="inner-div"/>
+      </Modal>);
+      expect(driver.getChildBySelector('[data-hook="inner-div"]')).not.toBeNull();
     });
 
+  });
+
+  describe('callbacks', () => {
     it(`should trigger the onAfterOpen function`, () => {
       props.onAfterOpen = sinon.spy();
+      console.log(props);
 
       createDriver(<Modal {...props}/>);
       expect(props.onAfterOpen.calledOnce).toBeTruthy();
     });
 
     it(`should trigger the onRequestClose function when clicking the overlay`, () => {
-      // props.onRequestClose = sinon.spy();
-      // props.shouldCloseOnOverlayClick = true;
-      // props.closeTimeoutMS = 0;
 
-      // const driver = createDriver(<Modal {...props}/>);
-      // driver.clickOnOverlay();
+      props.onRequestClose = sinon.spy();
+      props.shouldCloseOnOverlayClick = true;
+      props.closeTimeoutMS = 0;
 
-      // expect(props.onRequestClose.calledOnce).toBeTruthy();
+      const driver = createDriver(<Modal {...props}/>);
+      driver.clickOnOverlay();
 
+      expect(props.onRequestClose.calledOnce).toBeTruthy();
+    });
+
+    it(`should wait closeTimeoutMS before removing the modal`, done => {
+
+      props.shouldCloseOnOverlayClick = true;
+      props.closeTimeoutMS = 500;
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = props.closeTimeoutMS + 1000;
+
+      const driver = createDriver(<Modal {...props}/>);
+      driver.clickOnOverlay();
+
+      expect(driver.isOpen()).toBeTruthy();
+
+      setTimeout(() => {
+        expect(driver.isOpen()).toBeTruthy();
+        done();
+      }, props.closeTimeoutMS + 100);
+
+      setTimeout(() => {
+        expect(driver.isOpen()).toBeFalsy();
+        done();
+      }, props.closeTimeoutMS + 100);
     });
   });
 
-  // describe('testkit', () => {
-  //   it('should exist', () => {
-  //     expect(isTestkitExists(<Modal/>, modalTestkitFactory)).toBe(true);
-  //   });
-  // });
+  describe('theme attribute', () => {
+    it('should set the theme by default to "blue"', () => {
+      const driver = createDriver(<Modal {...props}/>);
+      expect(driver.isThemeExist('blue')).toBeTruthy();
+    });
 
-  // describe('enzyme testkit', () => {
-  //   it('should exist', () => {
-  //     expect(isEnzymeTestkitExists(<Modal/>, enzymeMessageBoxTestkitFactory)).toBe(true);
-  //   });
-  // });
+    it('should allowing setting the theme', () => {
+      props.theme = 'green';
+      const driver = createDriver(<Modal {...props}/>);
+      expect(driver.isThemeExist('green')).toBeTruthy();
+      expect(driver.isThemeExist('blue')).toBeFalsy();
+    });
+  });
+
+  describe('testkit', () => {
+    it('should exist', () => {
+      expect(isTestkitExists(<Modal {...props}/>, modalTestkitFactory)).toBe(true);
+    });
+  });
+
+  describe('enzyme testkit', () => {
+    it('should exist', () => {
+      expect(isEnzymeTestkitExists(<Modal/>, enzymeMessageBoxTestkitFactory)).toBe(true);
+    });
+  });
 
 
 });
